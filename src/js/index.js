eff545ebbe944f63457d746ee3025dd78cdde8aa
Neal <neal@webcheckout.net>
2019-11-01 14:39:52 -0500

    created Search model and controlSearch function with event listener on the search bar

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,32 @@
-import axios from 'axios'; 
 
-async function getResults(query){
-	try {
-		const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-		const recipes = res.data.recipes;
-		console.log(recipes)
-	} catch(error){
-		console.log(error)
-	}
+import Search from './models/Search';
+
+// Global state of the app
+// - search object
+// - current recipe object
+// - shoppoing list object
+// - liked recipes
+const state = {};
+
+const controlSearch = async() => {
+  // 1) get query from the view
+  const query = 'pizza'; //TODO
+
+  if(query){
+    // 2) new search object and add to state
+    state.search = new Search(query);
+
+    // 3) prepare UI for results
+
+    // 4) search for recipes
+    await state.search.getResults();
+
+    // 5) render results on UI
+    console.log(state.search.result);
+  }
 }
 
-getResults('bacon');
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
